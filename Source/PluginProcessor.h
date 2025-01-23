b9ae2b75c3ba2cfc26e529db987154df22b48e8f
ChristoNP <chris.pontoh@gmail.com>
2025-01-23 09:55:47 +0700

    adding freq, gain, slope for low freq, hi freq, and peak

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -53,11 +53,11 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
-    juce::AudioProcessorValueTreeState::ParameterLayout
+    static juce::AudioProcessorValueTreeState::ParameterLayout
         createParameterLayout();
-    juce::AudioProcessorValueTreeState apvts{
-        *this, nullptr, "Parameters", createParameterLayout()
-    };
+
+    juce::AudioProcessorValueTreeState apvts{ *this, nullptr,
+        "Parameters", createParameterLayout() };
 
 private:
     //==============================================================================
